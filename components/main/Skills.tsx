786cdbe4b17e5310f149f7ba65cdf40689dd3e62
francovillagra <fr4nconv@gmail.com>
2025-05-22 17:23:26 -0300

    Seccion de habilidades: creacion de componentes Skills y SkillDataProvider para estructurar y mostrar las tecnologias utilizadas

---
+++ components/main/Skills.tsx
@@ -0,0 +1,26 @@
+import { Frontend_skill } from '@/constants'
+import React from 'react'
+
+const Skills = () => {
+  return (
+    <section 
+    id="skills"
+    className="flex flex-col items-center justify-center gap-3 h-full relative overflow-hidden py-20"
+    style={{transform: "scale(0.9"}}
+    >
+            <div className="flex flex-row justify-around flex-wrap mt-4 gap-5 items-center">
+                {Frontend_skill.map((image, index) => (
+                    <SkillsDataProvider
+                    key={index}
+                        src={image.Image}
+                        width={image.width}
+                        heigth={image.height}
+                        index={index}
+                    />
+            ))}
+            </div>
+    </section>
+  )
+}
+
+export default Skills
