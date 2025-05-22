ab58ddc45f1769f67c7ffa09996268ee2a5a524b
francovillagra <fr4nconv@gmail.com>
2025-05-22 12:56:32 -0300

    Ajustes en layout global y Navbar: estructura visual mejorada y estilos actualizados

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
   <body 
     className={`${inter.className} ${openSans.className} bg-[#030014] overflow-y-scroll overflow-x-hidden`}>
     <StarsCanvas />
-
+    <Navbar />
     {children}
   </body>
 </html>
