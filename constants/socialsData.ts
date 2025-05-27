e193c9542dda186710f6c0739abf316b0b376690
francovillagra <fr4nconv@gmail.com>
2025-05-27 16:07:21 -0300

    Luego de modificar la forma de importar Redes Sociales en Navbar y crear el componente socialsData.ts, ahora modifique la estructura y agregue enlaces e iconos visuales en la web

--- constants/socialsData.ts
+++ constants/socialsData.ts
@@ -1,5 +1,24 @@
 export const Socials = [
-  { name: 'GitHub', src: '/github.png' },
-  { name: 'LinkedIn', src: '/linkedin.png' },
-  { name: 'Twitter', src: '/twitter.png' },
+  {
+    name: "GitHub",
+    src: "/github-142-white.svg", // Debería ser blanco
+    url: "https://github.com/tu-usuario",
+  },
+  {
+    name: "Instagram",
+    src: "/instagram.svg",
+    url: "https://instagram.com/tu-usuario",
+  },
+  
+  {
+    name: "LinkedIn",
+    src: "/linkedin.svg",
+    url: "https://linkedin.com/in/tu-usuario",
+  },
+ 
+  {
+    name: "Twitter",
+    src: "/twitter.svg",
+    url: "https://twitter.com/tu-usuario",
+  },
 ];
