ca04e3fc8a1ed496d5abf154903f6d48425d8b6e
francovillagra <fr4nconv@gmail.com>
2025-05-28 14:41:48 -0300

    modificando VideoOverlay para que el fondo quede fijo y el BlackHole no lo superponga

--- components/main/VideoOverlay.tsx
+++ components/main/VideoOverlay.tsx
@@ -1,18 +1,16 @@
 'use client';
 
-import React from 'react';
-
 const VideoOverlay = () => {
   return (
     <video
-      className="fixed inset-0 w-full h-full object-cover z-[-1] pointer-events-none"
+      className="fixed inset-0 w-full h-full object-cover z-10 pointer-events-none mix-blend-screen opacity-70"
       autoPlay
       loop
       muted
       playsInline
     >
       <source src="/blackhole.webm" type="video/webm" />
-      Mi navegador no soporta la etiqueta de video
+      Tu navegador no soporta la etiqueta de video.
     </video>
   );
 };
