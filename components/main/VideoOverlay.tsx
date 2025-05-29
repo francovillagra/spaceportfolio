90010b2ae2a178ee05b77b3e6e5acf1414f5d179
francovillagra <fr4nconv@gmail.com>
2025-05-29 18:46:26 -0300

    Probando una mascara CSS para recortar las partes negras del BlackHole y hacer que las estrellas se vean a traves del mismo. Primero ajuste el VideoOverlay.tsx, agregando lo siguiente en className: mask-blackhole. Luego agregue la clase mask-blackhole en mi CSS global, y lo coloque al final para evitar conflictos.

--- components/main/VideoOverlay.tsx
+++ components/main/VideoOverlay.tsx
@@ -2,16 +2,18 @@
 
 const VideoOverlay = () => {
   return (
-    <video
-      className="fixed top-0 left-0 w-full h-full object-cover z-[-1] pointer-events-none mix-blend-screen opacity-70"
-      autoPlay
-      loop
-      muted
-      playsInline
-    >
-      <source src="/blackhole.webm" type="video/webm" />
-      Tu navegador no soporta la etiqueta de video.
-    </video>
+    <div className="fixed top-0 left-0 w-full h-full z-[-1] pointer-events-none">
+      <video
+        className="w-full h-full object-cover mask-blackhole mix-blend-screen opacity-80"
+        autoPlay
+        loop
+        muted
+        playsInline
+      >
+        <source src="/blackhole.webm" type="video/webm" />
+        Tu navegador no soporta la etiqueta de video.
+      </video>
+    </div>
   );
 };
 
