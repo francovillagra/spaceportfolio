fc1ca5d6d037aa3a1c0000cdb1e74006394076bb
francovillagra <fr4nconv@gmail.com>
2025-06-02 13:00:56 -0300

    Refactor: Le agregue z-0 al video para que quede detras de los textos. Mask-Blackhole apliacada para la mascara CSS. Mix-blend-screen y opacity-80 para la mezcla de capas. Overflow-hidden al contenedor del video para evitar desbordes.

--- components/main/Encryption.tsx
+++ components/main/Encryption.tsx
@@ -1,65 +1,58 @@
 "use client"
 
 import React from 'react'
-
-import {motion} from 'framer-motion'
+import { motion } from 'framer-motion'
 import { slideInFromTop } from '@/utils/motion'
+import Image from 'next/image'
 
 const Encryption = () => {
   return (
-    <div className= 'flex flex-row relative items-center justify-center w-full h-full'>
-        <div className= 'absolute w-auto h-auto top-0 z-[-5]'>
-            <motion.div
-            variants={slideInFromTop}
-            className= 'text-[40px] font-medium text-center text-gray-200'
-            >
-                <span className="font-sans text-5xl font-bold">
-                        Performance  {" "}
-                <span className="font-sans text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500">
-                        &
-                </span>
-                    </span>
-                <span className="font-sans text-5xl font-bold">Seguridad</span>
+    <div className='relative w-full h-full flex items-center justify-center z-10'>
+      <div className='absolute top-0 z-10'>
+        <motion.div
+          variants={slideInFromTop}
+          className='text-[40px] font-medium text-center text-gray-200'
+        >
+          <span className="font-sans text-5xl font-bold">
+            Performance{" "}
+            <span className="font-sans text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500">
+              &
+            </span>
+          </span>
+          <span className="font-sans text-5xl font-bold">Seguridad</span>
+        </motion.div>
+      </div>
 
-            </motion.div>
-
+      <div className='absolute z-20 flex flex-col items-center justify-center translate-y-[-50px]'>
+        <div className='flex flex-col items-center group cursor-pointer'>
+          <Image
+            src="/LockTop.png"
+            alt='Lock top'
+            width={50}
+            height={50}
+            className='translate-y-5 transition-all duration-200 group-hover:translate-y-11'
+          />
+          <Image
+            src="/LockMain.png"
+            alt='Lock Main'
+            width={70}
+            height={70}
+            className='z-10'
+          />
         </div>
+      </div>
 
-        <div className= 'flex flex-col items-center justify-center translate-y-[-50px] absolute z-[20] w-auto h-auto'>
-            <div className='flex flex-col items-center group cursor-pointer w-auto h-auto'>
-                <Image
-                src="/LockTop.png"
-                alt='Lock top'
-                width={50}
-                height={50}
-                className='translate-y-5 transition-all duration-200 group-hove:translate-y-11'
-                />
-
-                <Image
-                src="/LockMain.png"
-                alt='Lock Main'
-                width={70}
-                height={70}
-                className='z-10'
-                />
-
-            </div>
-
-        </div>
-
-        <div className='w-full flex items-start justify-center absolute'>
-            <video 
-                loop
-                muted
-                autoPlay
-                playsInline
-                preload='false'
-                className='w-full h-auto'
-                src="/encryption.webm"
-            />
-
-        </div>
-
+      <div className='absolute w-full h-full z-0 overflow-hidden'>
+        <video 
+          loop
+          muted
+          autoPlay
+          playsInline
+          preload="none"
+          className='w-full h-full object-cover mask-blackhole opacity-80 mix-blend-screen'
+          src="/encryption.webm"
+        />
+      </div>
     </div>
   )
 }
