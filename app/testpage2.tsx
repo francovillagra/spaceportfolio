3ed9a7fcca0aeea0f59392802872db1926c73ede
francovillagra <fr4nconv@gmail.com>
2025-06-09 19:04:00 -0300

    Modifique postcss.config.js. Cree dos componentes de prueba.tsx para testear si Tailwindcss esta renderizando

---
+++ app/testpage2.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="bg-black text-white min-h-screen flex items-center justify-center">
+      <h1 className="text-3xl font-bold">Hola, Franco 👋</h1>
+    </main>
+  );
+}
