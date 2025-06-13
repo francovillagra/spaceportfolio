b936bde1fe88db3ca40099785059f17e31854748
francovillagra <fr4nconv@gmail.com>
2025-06-13 15:02:59 -0300

    Creando ramab para modificar desde cero los componentes y poder encontrar la raiz del problema. Elimino 3 componentes de prueba que no modifican en nada la estructura de la web

--- app/testpage2.tsx
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (
-    <main className="bg-black text-white min-h-screen flex items-center justify-center">
-      <h1 className="text-3xl font-bold">Hola, Franco 👋</h1>
-    </main>
-  );
-}
--- components/main/TestComponent.tsx
+++
@@ -1,5 +0,0 @@
-const TestComponent = () => {
-    return <div className='text-red-500'>Este es un componente de prueba</div>;
-  };
-  
-  export default TestComponent;
