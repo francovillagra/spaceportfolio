4fd213244d034baba033c729a7d9c6c7e947bd0e
francovillagra <fr4nconv@gmail.com>
2025-06-18 13:56:30 -0300

    Escibiendo el contenido de mi Projects.tsx y mi ProjectCard.tsx. Importando StarBackground.tsx en mi page.tsx, intentando que el fondo estelar se vea detras de mis ProjectsCards

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,23 @@
 import Encryption from "@/components/main/Encryption";
 import Hero from "@/components/main/Hero";
 import Skills from "@/components/main/Skills";
+import Projects from "@/components/main/Projects";
+import StarBackground from "@/components/main/StarBackground"; // Importá tu fondo estelar
 
 export default function Home() {
   return (
-    <main className="min-h-screen w-full">
-      <div className="flex flex-col gap-20">
-        <Hero />
-        <Skills />
-        <Encryption />
-      </div>
-    </main>
+    <>
+      {/* Fondo estelar */}
+      <StarBackground />
+
+      <main className="relative min-h-screen w-full z-10">
+        <div className="flex flex-col gap-20">
+          <Hero />
+          <Skills />
+          <Encryption />
+          <Projects />
+        </div>
+      </main>
+    </>
   );
 }
