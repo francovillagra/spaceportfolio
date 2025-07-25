b54b39ba227d52ba7e2bf14cb61a30599a83a0b5
francovillagra <fr4nconv@gmail.com>
2025-07-25 15:59:06 -0300

    Solucionando errores de importacion: Creando un script en la raiz del proyecto para buscar automaticamente errores de importacion. Tambien lo programe para que sugiera el path correcto con el casing real

--- components/sections/Navigation/HomeNavigation.tsx
+++ components/sections/Navigation/HomeNavigation.tsx
@@ -3,7 +3,7 @@
 import { useState } from 'react';
 import { FaBars, FaTimes } from 'react-icons/fa';
 import { NavigationItem, NavigationMenu } from '@/components/navigation';
-import Button from '@/components/ui/button';
+import Button from '@/components/ui/Button';
 
 type SectionKey = 'home' | 'about' | 'skills' | 'projects' | 'contact' | 'hero';
 
