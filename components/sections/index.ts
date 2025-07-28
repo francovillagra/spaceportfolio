6e147849c3789daa5fd56d87116e2ab1fc6aa81d
francovillagra <fr4nconv@gmail.com>
2025-07-28 14:42:01 -0300

    Corrigiendo error en Components/Sections/index.ts: En línea 1: export * from './About'; El error decia 'el archivo no es un módulo' Ese error se da si el archivo index.ts no tiene ninguna exportacion. Solucion: Primero genere un comando en git bash para listar las carpetas y archivos dentrro de la carpeta Sections:
    find component/Sections -type f \( -name '*.tsx' -o -name '*.ts'\(
    
    Ese comando va a mostrar todos los archivos .tsx y .ts dentro de components/Sections y subcarpetas.
    
    Gracias al listado, se armo el siguiente conteido del archivo:
    export * from './About';
    
    Aclaraciones importantes: (About, Contact, Hero, etc) tiene un archivo index.ts por lo tanto el enfoque que le doy es válido y limpio.
    El archivo page.tsx dentro de section no necesita estar exportado (es parte del enrutamiento de Next.js)
    Si en alguna carpeta no hay index.ts o esta vacion, podria tener un error. Pero con el listado , todas las mas importantes lo tienen.

--- components/sections/index.ts
+++ components/sections/index.ts
@@ -1,4 +1,7 @@
-export { default as AboutSection } from './About/AboutSection';
-export { default as ContactSection } from './Contact/ContactSection';
-export { default as HeroSection } from './Hero/HeroSection';
-export { default as ProjectsSection } from './Projects/ProjectsSection';
+export * from './About';
+export * from './Contact';
+export * from './Hero';
+export * from './Navigation';
+export * from './Projects';
+export * from './SectionLayouts';
+export * from './Skills';
