e16949c7421486f86e3b41bb13509deafa877cc7
francovillagra <fr4nconv@gmail.com>
2025-07-28 13:15:32 -0300

    Corrigiendo el contenido de index.ts que engloba los componentes dentro de carpeta Sections

--- components/sections/index.ts
+++ components/sections/index.ts
@@ -1,3 +1,4 @@
-export { default as AboutSection } from './About';
-export { default as ContactSection } from './ContactSection';
-
+export { default as AboutSection } from './About/AboutSection';
+export { default as ContactSection } from './Contact/ContactSection';
+export { default as HeroSection } from './Hero/HeroSection';
+export { default as ProjectsSection } from './Projects/ProjectsSection';
